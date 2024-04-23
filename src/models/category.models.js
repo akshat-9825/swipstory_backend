9ba9b4b81edf83aa1cd6042aa42dbef51a4ab400
Akshat Dwivedi <56993884+akshat-9825@users.noreply.github.com>
2024-04-23 12:35:30 +0530

    nit fix

--- src/models/category.models.js
+++ src/models/category.models.js
@@ -4,6 +4,9 @@ const categorySchema = new mongoose.Schema({
     category: {
         type: String,
     },
+    imageUrl: {
+        type: String,
+    },
 });
 
 module.exports = mongoose.model("Category", categorySchema);
